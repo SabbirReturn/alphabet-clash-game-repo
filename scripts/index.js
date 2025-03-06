4479c6129607eb4e26c3b98bae5a1bd7237107c1
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-03-07 04:01:27 +0600

    attach script file and connect home and playground section

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+document.getElementById('play').addEventListener('click', function(){
+    // let homeSection = document.getElementById('home');
+    // homeSection.classList.add('hidden')
+    // let playGroundSection = document.getElementById('play-ground');
+    // playGroundSection.classList.remove('hidden')
+    homeSection('home')
+    palyGroundSection('play-ground')
+})
---
+++ scripts/utility.js
@@ -0,0 +1,8 @@
+function homeSection(elementId){
+    let element = document.getElementById(elementId);
+    element.classList.add('hidden');
+}
+function palyGroundSection(elementId){
+    let playGround = document.getElementById(elementId);
+    playGround.classList.remove('hidden');
+}
